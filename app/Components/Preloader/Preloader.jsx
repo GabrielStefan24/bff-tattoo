e4150028d166c60b24a19f08710ed3b39808df48
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-09-02 11:21:05 +0300

    preloader

---
+++ app/Components/Preloader/Preloader.jsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+import styles from "./styles.module.scss";
+
+import { firstRow, secondRow, thirdRow } from "../../Data/imageData";
+const Preloader = () => {
+  return (
+    <section className={styles.tiles}>
+      <div className={styles.tile}>
+        {firstRow.map((img, index) => (
+          <div key={index} className={styles.tileImg}>
+            <img src={img.src} alt="image of a tattoo" />
+          </div>
+        ))}
+
+        <div className={styles.tileImg}>
+          <img src={firstRow[0].src} alt="image of a tattoo" />
+        </div>
+      </div>
+      <div className={styles.tile} style={{ transform: "translateX(-25%)" }}>
+        {secondRow.map((img, index) => (
+          <div key={index} className={styles.tileImg}>
+            <img src={img.src} alt="image of a tattoo" />
+          </div>
+        ))}
+
+        <div className={styles.tileImg}>
+          <img src={secondRow[0].src} alt="image of a tattoo" />
+        </div>
+      </div>
+      <div className={styles.tile}>
+        {thirdRow.map((img, index) => (
+          <div key={index} className={styles.tileImg}>
+            <img src={img.src} alt="image of a tattoo" />
+          </div>
+        ))}
+
+        <div className={styles.tileImg}>
+          <img src={thirdRow[0].src} alt="image of a tattoo" />
+        </div>
+      </div>
+      <div className={styles.gradient}></div>
+    </section>
+  );
+};
+
+export default Preloader;
---
+++ app/Data/imageData.js
@@ -0,0 +1,97 @@
+export const secondRow = [
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+];
+export const thirdRow = [
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+];
+
+export const firstRow = [
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+  {
+    src: "/kanye.jpeg",
+  },
+];
