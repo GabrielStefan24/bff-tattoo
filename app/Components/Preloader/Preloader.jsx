1c5d185e724eb1c13fccfecdfeebcf12f200d999
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-09-02 14:29:30 +0300

    somehow it worked

--- app/Components/Preloader/Preloader.jsx
+++ app/Components/Preloader/Preloader.jsx
@@ -28,21 +28,23 @@ const Preloader = () => {
               <img src={img.src} alt="image of a tattoo" />
             </div>
           ))}
-
-          <div className={styles.tileImg}>
-            <img src={firstRow[0].src} alt="image of a tattoo" />
-          </div>
+          {firstRow.map((img, index) => (
+            <div key={index} className={styles.tileImg}>
+              <img src={img.src} alt="image of a tattoo" />
+            </div>
+          ))}
         </div>
-        <div className={styles.tile} style={{ transform: "translateX(-25%)" }}>
+        <div className={styles.tile} style={{ transform: "translateX(-100%)" }}>
           {secondRow.map((img, index) => (
             <div key={index} className={styles.tileImg}>
               <img src={img.src} alt="image of a tattoo" />
             </div>
           ))}
-
-          <div className={styles.tileImg}>
-            <img src={secondRow[0].src} alt="image of a tattoo" />
-          </div>
+          {secondRow.map((img, index) => (
+            <div key={index} className={styles.tileImg}>
+              <img src={img.src} alt="image of a tattoo" />
+            </div>
+          ))}
         </div>
         <div className={styles.tile}>
           {thirdRow.map((img, index) => (
@@ -50,16 +52,18 @@ const Preloader = () => {
               <img src={img.src} alt="image of a tattoo" />
             </div>
           ))}
-
-          <div className={styles.tileImg}>
-            <img src={thirdRow[0].src} alt="image of a tattoo" />
-          </div>
+          {thirdRow.map((img, index) => (
+            <div key={index} className={styles.tileImg}>
+              <img src={img.src} alt="image of a tattoo" />
+            </div>
+          ))}
         </div>
       </motion.section>
       <div className={styles.gradient}>
         <motion.h1 variants={slideUp} initial="initial" animate="enter">
           Kanye
         </motion.h1>
+        <p>Click to enter</p>
       </div>
       {isLoading && <div className={styles.black}></div>}
     </>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 "use client";
+import { useState } from "react";
 import Preloader from "./Components/Preloader/Preloader";
 
 export default function Home() {
-  return (
-    <main className=" text-white">
-      <Preloader />
-    </main>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  return <main className=" text-white">{isLoading && <Preloader />}</main>;
 }
