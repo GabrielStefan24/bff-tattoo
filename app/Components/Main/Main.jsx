12c625ef5a23b1e7d31e5b1225e0eabd65c9bd9e
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-09-02 15:34:56 +0300

    fix state

---
+++ app/Components/Main/Main.jsx
@@ -0,0 +1,15 @@
+"use client";
+import styles from "./styles.module.scss";
+
+const Main = () => {
+  return (
+    <section className={styles.section}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
+      expedita voluptatibus nisi itaque. Recusandae, minima! Debitis, optio
+      error, velit dolorum rerum repudiandae illum voluptates eum, laudantium
+      tempora natus ullam impedit.
+    </section>
+  );
+};
+
+export default Main;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 "use client";
 import { useState } from "react";
 import Preloader from "./Components/Preloader/Preloader";
-
+import Main from "./Components/Main/Main";
+import { AnimatePresence } from "framer-motion";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
-  return <main className=" text-white">{isLoading && <Preloader />}</main>;
+  return (
+    <main className=" text-white">
+      <AnimatePresence>
+        {isLoading && <Preloader setIsLoading={setIsLoading} key="preloader" />}
+      </AnimatePresence>
+      {!isLoading && <Main />}
+    </main>
+  );
 }
