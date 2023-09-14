d758d66d62c5363949018cf0fa52d6d509120bc1
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-09-14 10:03:12 +0300

    Update index.jsx

--- app/Components/Artists/index.jsx
+++ app/Components/Artists/index.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import styles from "./styles.module.scss";
-import { useRef, useEffect} from "react";
+import { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -14,55 +14,59 @@ const Artists = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    let ctx = gsap.context(() => {
-      gsap.from(pair1Ref.current, {
-        y: "-100%",
-        opacity: 0,
-        scrollTrigger: {
-          trigger: pair1Ref.current,
-          start: "bottom bottom",
-          end: "center top",
-          scrub: 0.5,
-        },
-      });
-      gsap.from(pair2Ref.current, {
-        x: "-100%",
-        y: "150%",
-        scale: 0,
-        opacity: 0,
+    if (pair1Ref.current && pair2Ref.current && pair3Ref.current) {
+      let ctx = gsap.context(() => {
+        gsap.from(pair1Ref.current, {
+          y: "-100%",
+          opacity: 0,
+          scrollTrigger: {
+            trigger: pair1Ref.current,
+            start: "bottom bottom",
+            end: "center top",
+            scrub: 0.5,
+          },
+        });
+        gsap.from(pair2Ref.current, {
+          x: "-100%",
+          y: "150%",
+          scale: 0,
+          opacity: 0,
+          scrollTrigger: {
+            trigger: pair1Ref.current,
+            start: "top bottom",
+            end: "center top",
+            scrub: 0.5,
+          },
+        });
+        gsap.from(pair3Ref.current, {
+          x: "-150%",
+          y: "150%",
+          scale: 0,
+          opacity: 0,
+          scrollTrigger: {
+            trigger: pair1Ref.current,
+            start: "top bottom",
+            end: "center top",
+            scrub: 0.5,
+          },
+        });
 
-        scrollTrigger: {
-          trigger: pair1Ref.current,
-          start: "top bottom",
-          end: "center top",
-          scrub: 0.5,
-        },
-      });
-      gsap.from(pair3Ref.current, {
-        x: "-150%",
-        y: "150%",
-        scale: 0,
-        opacity: 0,
+        ScrollTrigger.refresh();
 
-        scrollTrigger: {
-          trigger: pair1Ref.current,
-          start: "top bottom",
-          end: "center top",
-          scrub: 0.5,
-        },
+        return () => {
+          gsap.killTweensOf([pair1Ref.current, pair2Ref.current, pair3Ref.current]);
+          ScrollTrigger.getAll().forEach(st => st.kill());
+          ctx.revert();
+        };
       });
-
-      return () => {
-        ctx.revert();
-      };
-    });
-  },[]);
+    }
+  }, []);
 
   return (
     <section className={styles.artists}>
       <div className={styles.imgContainer} ref={container}>
         <div className={styles.imgPair} ref={pair1Ref}>
-          <a href="https://www.instagram.com/ed_does" target="_blanc">
+          <a href="https://www.instagram.com/ed_does" target="_blank" rel="noopener noreferrer">
             <img src="/artist3.jpg" alt="photo of Eduard Stancu" />
           </a>
           <div className={styles.artistBio}>
@@ -71,7 +75,7 @@ const Artists = () => {
           </div>
         </div>
         <div className={styles.imgPair} ref={pair2Ref}>
-          <a href="https://www.instagram.com/russetattoo" target="_blanc">
+          <a href="https://www.instagram.com/russetattoo" target="_blank" rel="noopener noreferrer">
             <img src="/artist3.jpg" alt="photo of Radu" />
           </a>
           <div className={styles.artistBio}>
@@ -80,7 +84,7 @@ const Artists = () => {
           </div>
         </div>
         <div className={styles.imgPair} ref={pair3Ref}>
-          <a href="https://www.instagram.com/sevilens_craft" target="_blanc">
+          <a href="https://www.instagram.com/sevilens_craft" target="_blank" rel="noopener noreferrer">
             <img src="/artist3.jpg" alt="photo of Mihaela" />
           </a>
           <div className={styles.artistBio}>
