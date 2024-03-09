43ea01613d31bc3ff9a48780aac4471a6fabee77
Sirbu Gabriel <sirbugabriel97@gmail.com>
2024-03-09 11:23:24 +0200

    new pics

--- app/Data/artistData.js
+++ app/Data/artistData.js
@@ -1,24 +1,24 @@
 export const artistData = [
   {
     name: "Eduard Stancu",
-    src: "/artist3.webp",
-    src2: "/Men.webp",
+    src: "/Eduard.webp",
+    src2: "/Eduard.webp",
     path: "Eduard",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus impedit similique rerum libero autem dolorem quo suscipit. Sed libero necessitatibus saepe consequuntur adipisci inventore eius, quae cum labore? Ex, dolor.Fugit praesentium repellat delectus ex, sed quisquam, beatae esse ad repellendus reprehenderit obcaecati quasi iure quod ipsam quia facere rem qui nulla maiores ratione. Minus ipsum nobis iusto labore culpa?",
   },
   {
     name: "Mihaela Maier",
-    src: "/artist3.webp",
-    src2: "/Men.webp",
+    src: "/Mihaela.webp",
+    src2: "/Mihaela.webp",
     path: "Mihaela",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus impedit similique rerum libero autem dolorem quo suscipit. Sed libero necessitatibus saepe consequuntur adipisci inventore eius, quae cum labore? Ex, dolor.Fugit praesentium repellat delectus ex, sed quisquam, beatae esse ad repellendus reprehenderit obcaecati quasi iure quod ipsam quia facere rem qui nulla maiores ratione. Minus ipsum nobis iusto labore culpa?",
   },
   {
     name: "Radu Ruse",
-    src: "/artist3.webp",
-    src2: "/Men.webp",
+    src: "/Radu.webp",
+    src2: "/Radu.webp",
     path: "Radu",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus impedit similique rerum libero autem dolorem quo suscipit. Sed libero necessitatibus saepe consequuntur adipisci inventore eius, quae cum labore? Ex, dolor.Fugit praesentium repellat delectus ex, sed quisquam, beatae esse ad repellendus reprehenderit obcaecati quasi iure quod ipsam quia facere rem qui nulla maiores ratione. Minus ipsum nobis iusto labore culpa?",
